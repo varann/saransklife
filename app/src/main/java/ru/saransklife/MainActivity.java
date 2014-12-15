a33b0531f2c69942c8ab0398bc668695d956a20d
Anna Savinova <varannn@gmail.com>
2014-12-15 11:15:51 +0300

    Переделана загрузка картинок на главном экране

--- app/src/main/java/ru/saransklife/MainActivity.java
+++ app/src/main/java/ru/saransklife/MainActivity.java
@@ -1,19 +1,24 @@
 package ru.saransklife;
 
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.ViewSwitcher;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -22,33 +27,44 @@ import java.util.concurrent.TimeUnit;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends FragmentActivity {
 
-	@ViewById ImageSwitcher imageSwitcher;
+	@ViewById DrawerLayout drawerLayout;
+	@ViewById Toolbar toolbar;
+	@ViewById ImageView image1;
+	@ViewById ImageView image2;
+	@ViewById ImageView image3;
 
-	private int[] homeBgArray = {R.drawable.saransk_1, R.drawable.saransk_2, R.drawable.saransk_3};
 	private int currentIndex = 0;
 	private ScheduledExecutorService scheduleTaskExecutor;
 	private ScheduledFuture schedule;
 
+	private List<ImageView> images = new ArrayList<>();
+	private Animation inAnimation;
+	private Animation outAnimation;
+
+	public MainActivity() {
+	}
 
 	@AfterViews
 	void afterViews() {
-		imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+		toolbar.setTitle(R.string.main_title);
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMMM yyyy г.", new Locale("ru", "RU"));
+		toolbar.setSubtitle(simpleDateFormat.format(new Date()));
+		toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
 			@Override
-			public View makeView() {
-				ImageView view = new ImageView(MainActivity.this);
-				view.setScaleType(ImageView.ScaleType.CENTER_CROP);
-				view.setLayoutParams(new ImageSwitcher.LayoutParams(
-						ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-				return view;
+			public void onClick(View v) {
+				drawerLayout.openDrawer(Gravity.START);
 			}
 		});
 
-		Animation in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
-		in.setDuration(500);
-		Animation out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
-		out.setDuration(500);
-		imageSwitcher.setInAnimation(in);
-		imageSwitcher.setOutAnimation(out);
+		images.add(image1);
+		images.add(image2);
+		images.add(image3);
+
+		inAnimation = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+		inAnimation.setDuration(500);
+		outAnimation = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+		outAnimation.setDuration(500);
 
 		scheduleTaskExecutor = Executors.newScheduledThreadPool(1);
 	}
@@ -60,21 +76,57 @@ public class MainActivity extends FragmentActivity {
 		schedule = scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
 			@Override
 			public void run() {
-				updateBg();
+				updateImage();
 			}
 		}, 0, 5, TimeUnit.SECONDS);
+	}
+
+	@UiThread
+	void updateImage() {
+		final ImageView oldImage = images.get(currentIndex);
+
+		outAnimation.setAnimationListener(new Animation.AnimationListener() {
+			@Override
+			public void onAnimationStart(Animation animation) {
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				oldImage.setVisibility(View.INVISIBLE);
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+			}
+		});
+		oldImage.startAnimation(outAnimation);
+
+		currentIndex++;
+		currentIndex = currentIndex % images.size();
+
+		final ImageView newImage = images.get(currentIndex);
+
+		inAnimation.setAnimationListener(new Animation.AnimationListener() {
+			@Override
+			public void onAnimationStart(Animation animation) {
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				newImage.setVisibility(View.VISIBLE);
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+			}
+		});
+		newImage.startAnimation(inAnimation);
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 		schedule.cancel(true);
-	}
-
-	@UiThread
-	void updateBg() {
-		imageSwitcher.setImageResource(homeBgArray[(currentIndex % homeBgArray.length)]);
-		currentIndex++;
 	}
 
 }
